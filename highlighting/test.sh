dc5fe01c23437334a0ce9582b52f0eff9d1c3558
Jakob Voss <voss@gbv.de>
2024-05-16 21:23:40 +0200

    Extend syntax highlighting

--- highlighting/test.sh
+++ highlighting/test.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+# requires
+# - skylighting
+# - kate-syntax-highlighter (install with Debian package libkf5syntaxhighlighting-tools)
+
 for pg in examples/*.pg; do
-  expect=${pg%.pg}.highlighting
+  expect=${pg%.pg}.skylighting
+  ansi=${pg%.pg}.ansi
 
   if [[ -f "$expect" ]]; then
     tmp=${pg%.pg}.tmp
     skylighting -d pg.xml -s pg -f native "$pg" 2>/dev/null > "$tmp"
+    # kate-syntax-highlighter -f ansi "$pg" > "$ansi"
 
     if  cmp -s "$tmp" "$expect" ; then
       echo "$pg - OK"
       rm "$tmp"
     else
       echo "$pg - highlighting not as expected"
-      cat $tmp
-      cat $expect
+      diff $tmp $expect
     fi
   else
     echo "$pg - created $expect"
     skylighting -d pg.xml -s pg -f native "$pg" 2>/dev/null > "$expect"
-    cat $expect
   fi
 done
